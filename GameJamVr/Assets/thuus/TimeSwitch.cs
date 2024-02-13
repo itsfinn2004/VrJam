fa20874e1d15b48a349aa488347b1c745945d717
Finn <fstreunding@gmail.com>
2024-02-13 14:56:10 +0100

    frsewfeffewfwfsfwfswfswfsfsgvdf

--- GameJamVr/Assets/thuus/TimeSwitch.cs
+++ GameJamVr/Assets/thuus/TimeSwitch.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+
 
 public class TimeSwitch : MonoBehaviour
 {
@@ -9,32 +11,41 @@ public class TimeSwitch : MonoBehaviour
     public GameObject player;
     public GameObject past;
     public GameObject future;
-    [SerializeField] InputActionReference TimeButton;
-
-
-   private bool time
+    [SerializeField]
+    InputActionProperty m_ActivateAction = new InputActionProperty(new InputAction("Activate", type: InputActionType.Button));
+    public InputActionProperty activateAction
     {
-        get => TimeButton;
+        get => m_ActivateAction;
         set
         {
             ChangeTime();
         }
     }
-
     public void ChangeTime()
     {
+        print("action");
         if (Future)
         {
             player.transform.position = future.transform.position;
-            Future = false;
             Debug.Log("past");
+            Future = !Future;
         }
         else
         {
             player.transform .position = past.transform.position;
-            Future = true;
             Debug.Log("future");
+            Future = !Future;
         }
 
+    }
+    void SetInputActionProperty(ref InputActionProperty property, InputActionProperty value)
+    {
+        if (Application.isPlaying)
+            property.DisableDirectAction();
+
+        property = value;
+
+        if (Application.isPlaying && isActiveAndEnabled)
+            property.EnableDirectAction();
     }
 }
